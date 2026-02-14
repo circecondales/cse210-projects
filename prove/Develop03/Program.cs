c114d058f6203d435c426fe45719608b5d578d07
circe condales <circec@byui.edu>
2026-02-14 00:39:36 -0400

    update

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,8 +4,7 @@ class Program //This is where we bring everything together so that the user can 
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
-        // 1. Inicialización
+       // 1. Inicialización
         Reference reference = new Reference("Proverb", 3, 5);
         Scriptures scriptures = new Scriptures(reference, "Trust in the LORD with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
         string input = " ";
